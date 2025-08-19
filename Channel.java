dc086a08789a2c0af7658cd1a569c9b5bceeb23c
Natalie <147546336+Natalie055@users.noreply.github.com>
2025-08-19 15:53:34 +0000

    commit

---
+++ Channel.java
@@ -0,0 +1,10 @@
+package HauntedRadio;
+
+public abstract class Channel {
+    protected int number;
+    protected String name;
+
+    public Channel(int number, String name) {
+        
+    }
+}
---
+++ GhostChannel.java
@@ -0,0 +1,10 @@
+package Hauntedradio;
+
+public class GhostChannel extends Channel {
+    public GhostChannel(int number) {
+        super(number, "Ghost Signal");
+    }
+    @Overridepublic void broadcast() {
+        System.our.println("A ghostly voice emerge: " + "WELCOME TO THE HIDDEN FREQUENCY...'\n")
+    }
+}
